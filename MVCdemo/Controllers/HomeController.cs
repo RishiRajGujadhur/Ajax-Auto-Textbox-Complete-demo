b17cafdd3fce496f165a593a064e7891e2388baa
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2016-07-15 21:54:31 +0400

    AJAX auto complete textbox
    ASP.net MVC app

---
+++ MVCdemo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCdemo.Models;
+
+namespace MVCdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+    public ActionResult Index()
+    {
+       howtoDBEntities db = new howtoDBEntities();
+        return View(db.Students);
+    }
+
+    [HttpPost]
+    public ActionResult Index(string searchTerm)
+    {
+        howtoDBEntities db = new howtoDBEntities();
+        List<Student> students;
+        if (string.IsNullOrEmpty(searchTerm))
+        {
+            students = db.Students.ToList();
+        }
+        else
+        {
+            students = db.Students
+                .Where(s => s.Name.StartsWith(searchTerm)).ToList();
+        }
+        return View(students);
+    }
+
+    public JsonResult GetStudents(string term)
+    {
+        howtoDBEntities db = new howtoDBEntities();
+        List<string> students = db.Students.Where(s => s.Name.StartsWith(term))
+            .Select(x => x.Name).ToList();
+        return Json(students, JsonRequestBehavior.AllowGet);
+    }
+}
+}
---
+++ MVCdemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCdemo.MvcApplication" Language="C#" %>
